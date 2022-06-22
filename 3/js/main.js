199934e90533c1b9e7b536fbc53598bb5828facb
Keks <keks@htmlacademy.ru>
2022-06-22 22:23:48 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,17 +1,18 @@
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    if (min >= max || min < 0){
+        return -1;
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);  
 }
-getRandomNumber(1, 100);
-console.log(getRandomNumber(1, 100));
+console.log(getRandomNumber(1,30));
+
+
 
 function checkStringLength (stringLength, maxLength){
-  maxLength = 140;
-  stringLength = 'string test';
   if (stringLength.length >= maxLength) {
-    return console.log(false);
-  } else {
-    return console.log(true);
-  }
-
+    return false;
+  } 
+  return true;
 }
-checkStringLength();
+checkStringLength('test', 140);
+
