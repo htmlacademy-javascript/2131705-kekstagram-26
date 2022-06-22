f27588b2c2c6a140317635366a812740a045c755
Subbotin Aleksey <kofta.print@yandex.ru>
2022-06-22 22:19:50 +0300

    внес правки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomNumber(min, max) {
     if (min >= max || min < 0){
-        return 'ошибка';
+        return -1;
     }
     return Math.floor(Math.random() * (max - min + 1) + min);  
 }
@@ -10,12 +10,9 @@ console.log(getRandomNumber(1,30));
 
 function checkStringLength (stringLength, maxLength){
   if (stringLength.length >= maxLength) {
-    console.log(false);
     return false;
   } 
-  console.log(true);
   return true;
-  
 }
 checkStringLength('test', 140);
 
